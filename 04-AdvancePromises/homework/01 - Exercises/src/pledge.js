68a6a91a9946d67b31e5f6a3645bd67ff5765acb
RolandoRp <rolyrp@hotmail.com>
2023-07-14 00:31:35 -0600

    advancedpromises

--- 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
+++ 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
@@ -5,10 +5,11 @@ Promises Workshop: construye la libreria de ES6 promises, pledge.js
 // // TU CÓDIGO AQUÍ:
 class $Promise{
     constructor(executor){
-        if(typeof executor !== 'function') throw
-        TypeError('Executor must be a function');
+        if(typeof executor !== 'function') throw TypeError
+        ('Executor must be a function');
         this._state = 'pending'
         this._value = undefined
+        this._handlerGroups = []
 
         const resolve = (value) => {
             this._internalResolve(value)
@@ -25,12 +26,46 @@ class $Promise{
         if(this._state !== 'pending') return;
         this._state = 'fulfilled'
         this._value = value
+        this._callHandlers(this._value)
     }
 
     _internalReject = (reason) => {
         if(this._state !== 'pending') return;
         this._state = 'rejected'
         this._value = reason
+        this._callHandlers(this._value)
+    }
+
+    then = (successCb, errorCb) => {
+        const handlerGroup = {
+            successCb: typeof successCb === 'function' ?
+            successCb : false,
+            errorCb: typeof errorCb === 'function' ?
+            errorCb : false
+        }
+
+        this._handlerGroups.push(handlerGroup)
+        this._state !== 'pending' && this._callHandlers
+        (this._value)
+    }
+
+    catch = (errorCb) => {
+        this.then(null, errorCb)
+    }
+
+    _callHandlers = (value) => {
+        while(this._handlerGroups.length){
+            const currentHandler = this._handlerGroups.
+            shift();
+
+            this._state === 'fulfilled' 
+            && currentHandler.successCb 
+            && currentHandler.successCb(value)
+
+            this._state === 'rejected' 
+            && currentHandler.errorCb 
+            && currentHandler.errorCb(value)
+        }
     }
 }
 
